1c88fec7e08ede36880eb91167a1406757079060
klehloenya <keketsolehloenya516@gmail.com>
2025-02-07 15:02:17 +0200

    Delete Frontend directory

--- Frontend/script.js
+++
@@ -1,6 +0,0 @@
-// Example: Add interactivity for the search bar
-document.querySelector('.search-bar button').addEventListener('click', function () {
-    const searchQuery = document.querySelector('.search-bar input').value;
-    alert(`You searched for: ${searchQuery}`);
-    // You can add logic to filter parts based on the search query
-  });
